aee9a4121e07b2630ee4030b4ae22581acfaf46b
amine <amineemails@gmail.com>
2024-03-29 07:16:53 +0100

    Loading page content from the database - Part Two

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-from .models import Product, Slider
+from .models import Product, Slider, Category
 
 def index(request):
     products = Product.objects.select_related('author').filter(featured=True)
@@ -12,8 +13,21 @@ def index(request):
 
 def product(request, pid):
     return render(request, 'product.html')
-def category(request, cid):
-    return render(request, 'category.html')
+def category(request, cid=None):
+    cat = None
+    where = {}
+    if cid:
+        cat = Category.objects.get(pk=cid)
+        where['category_id'] = cid
+    products = Product.objects.filter(**where)
+    paginator = Paginator(products, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'category.html', {
+        'page_obj': page_obj,
+        'category': cat
+    })
 def cart(request):
     return render(request, 'cart.html')
 def checkout(request):
